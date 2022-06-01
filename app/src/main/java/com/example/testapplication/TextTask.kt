ec17ee9e4d2fcd9d70e7f203c21d2fc9f7fb7eb0
Robkarrs <72616493+Robkarrs@users.noreply.github.com>
2022-06-01 14:48:37 +0200

    progress

--- app/src/main/java/com/example/testapplication/TextTask.kt
+++ app/src/main/java/com/example/testapplication/TextTask.kt
@@ -1,23 +1,37 @@
 package com.example.testapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.io.File
 
 val textList = arrayOf("This is the first test message","This is the second test message")
 var count = 0
+var backspaceCount = 0
 class TextTask  : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val filepath = "MyFileStorage"
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.text_task)
         val displayText = findViewById<TextView>(R.id.textDisplay)
         displayText.text = textList[count]
+
+       val buttonText = findViewById<Button>(R.id.button)
+       buttonText?.setOnClickListener()
+       {
+           storeMessage()
+       }
     }
 
-    /** Called when the user taps the Send button */
-    fun sendMessage(view: View) {
+    fun countBackspace()
+    {
+
+    }
+    fun storeMessage() {
         //Show next line
         count++
         val displayText = findViewById<TextView>(R.id.textDisplay)
@@ -31,7 +45,17 @@ class TextTask  : AppCompatActivity(){
         //Compare message and displayed text
 
         //Adjust scoring
-
+        //Compare string differences
+        //Store File
+        val file = File(getExternalFilesDir(filepath), datetime.toString())
+        //val path = Environment.getExternalStorageDirectory()
+        println(getExternalFilesDir(filepath))
+        file.createNewFile()
+        try {
+            file.appendText(message + " ")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
